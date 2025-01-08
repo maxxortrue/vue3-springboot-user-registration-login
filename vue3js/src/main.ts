1f165bb91f0a64ae4239f190d71c7aa457aaebbf
max <mwang98@hawk.iit.edu>
2025-01-08 18:17:40 +0800

    Adding file upload and download!

--- vue3js/src/main.ts
+++ vue3js/src/main.ts
@@ -3,14 +3,17 @@ import App from './App.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import RegisterComponent from './components/RegisterComponent.vue';
 import LoginComponent from './components/LoginComponent.vue';
+import TransferComponent from './components/TransferComponent.vue';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 
 // 定义路由
 const routes = [
-  { path: '/', redirect: '/register' },  // 根路径重定向到注册页面
+  { path: '/', redirect: '/transfer' },  // 根路径重定向到注册页面
   { path: '/register', name: 'Register', component: RegisterComponent },
-  { path: '/login', name: 'Login', component: LoginComponent }
+  { path: '/login', name: 'Login', component: LoginComponent },
+  { path: '/transfer', name: 'Login', component: TransferComponent }
 ];
 
 // 创建路由实例
